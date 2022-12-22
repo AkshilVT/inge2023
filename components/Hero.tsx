3583ff1476a6e3fc16449d4c369508a8cd1cc7c1
Akshil Thumar <akshilvthumar@gmail.com>
2022-12-22 18:17:51 +0530

    hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,41 @@
+import dynamic from 'next/dynamic';
+import Image from 'next/image';
+import React from 'react';
+// import Timer from './Timer';
+const Timer = dynamic(() => import('./Timer'), { ssr: false });
+function Hero() {
+  return (
+    <div className='max-w-screen-2xl flex flex-col justify-around items-center w-full'>
+      <aside className='space-y-4 w-full text-right'>
+        <h2 className='font-itc-avant text-xl tracking-[.4em] text-[#788596] uppercase'>
+          Join us at
+        </h2>
+        <h1 className='font-itc-serif uppercase tracking-widest text-3xl'>
+          28<sup>th</sup> - 29<sup>th</sup> of Jan 2023
+        </h1>
+        <h3 className='font-aurebesh text-sm tracking-[1.33em] text-[#788596]'>
+          Made by yours
+        </h3>
+      </aside>
+      <div className='relative w-full text-center mt-32'>
+        <h2 className='text-7xl scale-125 opacity-10 font-star-hollow tracking-widest absolute -top-10  w-full'>
+          ingenious HACKATHoN
+        </h2>
+        <h2 className='text-5xl font-star tracking-widest'>
+          ingenious HACKATHoN
+        </h2>
+      </div>
+      <div className='space-x-2 mt-32'>
+        <Image
+          src='/saber-green.png'
+          width={700}
+          height={700}
+          alt={'Green Saber'}
+        />
+        <Timer deadline='2023-01-28' />
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
