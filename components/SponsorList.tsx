815ec143480fa82422e8d7ec94d17f899a793c40
Akshil Thumar <akshilvthumar@gmail.com>
2022-12-28 11:31:13 +0530

    updated sponsors

--- components/SponsorList.tsx
+++ components/SponsorList.tsx
@@ -8,17 +8,15 @@ function SponsorList() {
             <div
                 className={
                     ' p-4 border-white ' +
-                    (open === 1 ? 'w-full border-4' : 'w-1/6 border-r-4')
+                    (open === 1
+                        ? 'w-full border-4 bg-white/10'
+                        : 'w-1/6 border-r-4 hover:bg-white/10')
                 }
+                onClick={() => {
+                    open === 1 ? setOpen(0) : setOpen(1)
+                }}
             >
-                <h1
-                    className="sponsor text-3xl cursor-pointer "
-                    onClick={() => {
-                        open === 1 ? setOpen(0) : setOpen(1)
-                    }}
-                >
-                    Title
-                </h1>
+                <h1 className="sponsor text-3xl cursor-pointer ">Title</h1>
                 {open === 1 && (
                     <div className="w-full grid grid-cols-3 items-center">
                         <Image
@@ -48,17 +46,15 @@ function SponsorList() {
             <div
                 className={
                     ' p-4 border-white ' +
-                    (open === 2 ? 'w-full border-4' : 'w-1/6 border-r-4')
+                    (open === 2
+                        ? 'w-full border-4 bg-white/10'
+                        : 'w-1/6 border-r-4 hover:bg-white/10')
                 }
+                onClick={() => {
+                    open === 2 ? setOpen(0) : setOpen(2)
+                }}
             >
-                <h1
-                    className="sponsor text-3xl cursor-pointer"
-                    onClick={() => {
-                        open === 2 ? setOpen(0) : setOpen(2)
-                    }}
-                >
-                    Associate
-                </h1>
+                <h1 className="sponsor text-3xl cursor-pointer">Associate</h1>
                 {open === 2 && (
                     <div className="w-full grid grid-cols-3 items-center">
                         <Image
@@ -88,17 +84,15 @@ function SponsorList() {
             <div
                 className={
                     ' p-4 border-white ' +
-                    (open === 3 ? 'w-full border-4' : 'w-1/6 border-r-4')
+                    (open === 3
+                        ? 'w-full border-4 bg-white/10'
+                        : 'w-1/6 border-r-4 hover:bg-white/10')
                 }
+                onClick={() => {
+                    open === 3 ? setOpen(0) : setOpen(3)
+                }}
             >
-                <h1
-                    className="sponsor text-3xl cursor-pointer"
-                    onClick={() => {
-                        open === 3 ? setOpen(0) : setOpen(3)
-                    }}
-                >
-                    Platinum
-                </h1>
+                <h1 className="sponsor text-3xl cursor-pointer">Platinum</h1>
                 {open === 3 && (
                     <div className="w-full grid grid-cols-3 items-center">
                         <Image
@@ -128,17 +122,15 @@ function SponsorList() {
             <div
                 className={
                     ' p-4 border-white ' +
-                    (open === 4 ? 'w-full border-4' : 'w-1/6 border-r-4')
+                    (open === 4
+                        ? 'w-full border-4 bg-white/10'
+                        : 'w-1/6 border-r-4 hover:bg-white/10')
                 }
+                onClick={() => {
+                    open === 4 ? setOpen(0) : setOpen(4)
+                }}
             >
-                <h1
-                    className="sponsor text-3xl cursor-pointer"
-                    onClick={() => {
-                        open === 4 ? setOpen(0) : setOpen(4)
-                    }}
-                >
-                    Diamond
-                </h1>
+                <h1 className="sponsor text-3xl cursor-pointer">Diamond</h1>
                 {open === 4 && (
                     <div className="w-full grid grid-cols-3 items-center">
                         <Image
@@ -168,17 +160,15 @@ function SponsorList() {
             <div
                 className={
                     ' p-4 border-white ' +
-                    (open === 5 ? 'w-full border-4' : 'w-1/6 border-r-4')
+                    (open === 5
+                        ? 'w-full border-4 bg-white/10'
+                        : 'w-1/6 border-r-4 hover:bg-white/10')
                 }
+                onClick={() => {
+                    open === 5 ? setOpen(0) : setOpen(5)
+                }}
             >
-                <h1
-                    className="sponsor text-3xl cursor-pointer"
-                    onClick={() => {
-                        open === 5 ? setOpen(0) : setOpen(5)
-                    }}
-                >
-                    Gold
-                </h1>
+                <h1 className="sponsor text-3xl cursor-pointer">Gold</h1>
                 {open === 5 && (
                     <div className="w-full grid grid-cols-3 items-center">
                         <Image
@@ -208,17 +198,15 @@ function SponsorList() {
             <div
                 className={
                     ' p-4 border-white ' +
-                    (open === 6 ? 'w-full border-4' : 'w-1/6 border-r-4')
+                    (open === 6
+                        ? 'w-full border-4 bg-white/10'
+                        : 'w-1/6 border-r-4 hover:bg-white/10')
                 }
+                onClick={() => {
+                    open === 6 ? setOpen(0) : setOpen(6)
+                }}
             >
-                <h1
-                    className="sponsor text-3xl cursor-pointer"
-                    onClick={() => {
-                        open === 6 ? setOpen(0) : setOpen(6)
-                    }}
-                >
-                    Silver
-                </h1>
+                <h1 className="sponsor text-3xl cursor-pointer">Silver</h1>
                 {open === 6 && (
                     <div className="w-full grid grid-cols-3 items-center">
                         <Image
@@ -248,17 +236,15 @@ function SponsorList() {
             <div
                 className={
                     ' p-4 border-white ' +
-                    (open === 7 ? 'w-full border-4' : 'w-1/6 border-r-4')
+                    (open === 7
+                        ? 'w-full border-4 bg-white/10'
+                        : 'w-1/6 border-r-4 hover:bg-white/10')
                 }
+                onClick={() => {
+                    open === 7 ? setOpen(0) : setOpen(7)
+                }}
             >
-                <h1
-                    className="sponsor text-3xl cursor-pointer"
-                    onClick={() => {
-                        open === 7 ? setOpen(0) : setOpen(7)
-                    }}
-                >
-                    Bronze
-                </h1>
+                <h1 className="sponsor text-3xl cursor-pointer">Bronze</h1>
                 {open === 7 && (
                     <div className="w-full grid grid-cols-3 items-center">
                         <Image
