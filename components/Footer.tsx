f8fbcd1af6b8df136d5af16b1c5ca6a44d27335c
Akshil Thumar <akshilvthumar@gmail.com>
2022-12-29 17:57:32 +0530

    linked

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div className="w-full max-w-screen-2xl h-max">
+        <div className="w-full max-w-screen-2xl ">
             <div className="border-8 w-full shadow-[0_0px_7.5px_0px_rgba(255,255,255,5)]"></div>
             <div className="h-full flex flex-col justify-between items-center w-full p-10 relative bg-[url('/footer.png')] bg-no-repeat bg-cover bg-center md:bg-left">
                 <div className="space-y-4 font-itc-avant max-w-[450px] my-8">
@@ -22,17 +22,76 @@ function Footer() {
                 </div>
                 <div className="font-itc-avant flex flex-col w-full">
                     <h1 className="text-3xl text-white/50 h-20">CONTACT US</h1>
-                    <div className="grid grid-cols-2 text-center gap-8">
-                        <p className="p-4 border border-white/40">Mail</p>
-                        <p className="p-4 border border-white/40">Insta</p>
-                        <p className="p-4 border border-white/40">kavan</p>
-                        <p className="p-4 border border-white/40">prasan</p>
+                    <div className="relative h-[300px] lg:h-[200px]">
+                        <div className="absolute z-20 inset-0">
+                            <div className="grid grid-cols-2 lg:grid-cols-3 text-center gap-8">
+                                <a
+                                    className="p-4 border border-white/40 hover:bg-white/40 cursor-pointer"
+                                    href="mailto:ingenium@ahduni.edu.in"
+                                >
+                                    Mail
+                                </a>
+                                <a
+                                    href="https://www.linkedin.com/in/tech-ingenium/"
+                                    target="blank"
+                                    rel="noopener noreferrer"
+                                    className="p-4 border border-white/40 hover:bg-white/40 cursor-pointer"
+                                >
+                                    LinkedIn
+                                </a>
+                                <a
+                                    className="p-4 border border-white/40 hover:bg-white/40 cursor-pointer"
+                                    target="blank"
+                                    rel="noopener noreferrer"
+                                    href="https://discord.gg/SWy9zmpj5r"
+                                >
+                                    Discord
+                                </a>
+                                <a
+                                    href="https://www.instagram.com/tech.ingenium/?hl=en"
+                                    target="blank"
+                                    rel="noopener noreferrer"
+                                    className="p-4 border border-white/40 hover:bg-white/40 cursor-pointer"
+                                >
+                                    Insta
+                                </a>
+                                <a
+                                    href="tel:+91-8320161492"
+                                    className="p-4 border border-white/40 hover:bg-white/40 cursor-pointer"
+                                >
+                                    kavan
+                                </a>
+                                <a
+                                    href="tel:+91-7220020004"
+                                    className="p-4 border border-white/40 hover:bg-white/40 cursor-pointer"
+                                >
+                                    prasan
+                                </a>
+                            </div>
+                            <h2 className="text-xl font-itc-serif text-white/70 text-center mt-4">
+                                Designed & Developed by{' '}
+                                <span className="px-2 underline hover:text-white cursor-pointer">
+                                    <a
+                                        href="https://github.com/AkshilVT"
+                                        target="blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        Akshil Thumar
+                                    </a>
+                                </span>
+                                &
+                                <span className="px-2 underline hover:text-white cursor-pointer">
+                                    <a
+                                        href="https://github.com/priyajani028"
+                                        target="blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        Priya Jani
+                                    </a>
+                                </span>
+                            </h2>
+                        </div>
                     </div>
-                    <h2 className="text-xl font-itc-serif text-white/70 text-center mt-4">
-                        Designed & Developed by{' '}
-                        <span className="px-2 underline">Akshil Thumar</span>&
-                        <span className="px-2 underline">Priya Jani</span>
-                    </h2>
                 </div>
             </div>
         </div>
